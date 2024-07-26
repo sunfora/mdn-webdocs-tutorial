edd3df3bca500844188f45ef79b3adeb84f18c16
kurku <sandovin@mail.ru>
2024-07-26 18:20:39 +0400

    probably done

--- 04 javascript/01 js-first-steps/10 silly story generator/main.js
+++ 04 javascript/01 js-first-steps/10 silly story generator/main.js
@@ -1,1 +1,94 @@
 "use strict";
+
+const customName = document.getElementById('customname');
+const randomize = document.querySelector('.randomize');
+const story = document.querySelector('.story');
+
+function randomValueFromArray(array){
+  const random = Math.floor(Math.random()*array.length);
+  return array[random];
+}
+
+const storyText = "It was :temperature: outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. :name: saw the whole thing, but was not surprised — :insertx: weighs :weight:, and it was a hot day.";
+
+const insertX = [
+  "Willy the Goblin",
+  "Big Daddy"       ,
+  "Father Christmas"
+];
+
+const insertY = [
+  "the soup kitchen",
+  "Disneyland"      ,
+  "the White House"
+];
+
+const insertZ = [
+  "spontaneously combusted",
+  "melted into a puddle on the sidewalk",
+  "turned into a slug and crawled away"
+];
+
+randomize.addEventListener('click', result);
+
+
+function temperature(fahrenheit, country) {
+  const celcius = F => Math.round((F - 32) * 5 / 9);
+
+  switch (country) {
+    case "us":
+      return `${fahrenheit} fahrenheit`;
+    case "uk":
+      return `${celcius(fahrenheit)} celcius`;
+    default:
+      throw new Error(`Country ${country} is not supported`);
+  }
+}
+
+function weight(lbs, country) {
+  const metricFactor = 0.453592;
+
+  const suffix = (weight) => {
+    return weight % 10 === 1? "": "s";
+  }
+
+  switch (country) {
+    case "us": {
+      const weight = lbs;
+      return `${weight} pound${suffix(weight)}`;
+    }
+    case "uk": {
+      const weight = Math.round(metricFactor * lbs);
+      return `${weight} kilogram${suffix(weight)}`;
+    }
+    default:
+      throw new Error(`Country ${country} is not supported`);
+  }
+}
+
+function substitute(text, params) {
+  for (const param in params) {
+    text = text.replaceAll(`:${param}:`, params[param].toString());
+  }
+  return text;
+}
+
+
+function result() {
+  const locale = document.getElementById("uk").checked? "uk" : "us";
+  const params = {
+    name: "Bob",
+    temperature: temperature(94, locale),
+    weight: weight(300, locale),
+    insertx: randomValueFromArray(insertX),
+    inserty: randomValueFromArray(insertY),
+    insertz: randomValueFromArray(insertZ)
+  };
+
+  if(customName.value !== '') {
+    params.name = customName.value;
+  }
+
+  story.textContent = substitute(storyText, params);
+  story.style.visibility = 'visible';
+}
