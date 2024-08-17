c65cf45a97c4194911141fe26dc0024ec6c0f491
kurku <sandovin@mail.ru>
2024-08-18 02:52:41 +0400

    promise 'callback hell' version

--- 04 javascript/04 async/06 sequencing-animations/main.js
+++ 04 javascript/04 async/06 sequencing-animations/main.js
@@ -12,3 +12,9 @@ const aliceTiming = {
 const alice1 = document.querySelector("#alice1");
 const alice2 = document.querySelector("#alice2");
 const alice3 = document.querySelector("#alice3");
+
+alice1.animate(aliceTumbling, aliceTiming).finished
+  .then(
+        () => alice2.animate(aliceTumbling, aliceTiming).finished
+                .then(
+                  () => alice3.animate(aliceTumbling, aliceTiming)));
