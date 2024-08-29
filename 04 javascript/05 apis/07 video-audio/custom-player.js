a36464acbb8e0ba3afa8d9189890fc78b449095a
kurku <sandovin@mail.ru>
2024-08-30 01:09:35 +0300

    add audio support + rework
    so basically I wrapped all the code into a function
    and now it doesn't matter which type of a content it is
    
    the other notable changes are within css
    the problem was absolute positioning, which doesn't work quite well
    with multiple players on the same page

--- 04 javascript/05 apis/07 video-audio/custom-player.js
+++ 04 javascript/05 apis/07 video-audio/custom-player.js
@@ -1,223 +1,231 @@
-const media = document.querySelector("video");
-const controls = document.querySelector(".controls");
 
-const play = document.querySelector(".play");
-const stop = document.querySelector(".stop");
-const rwd = document.querySelector(".rwd");
-const fwd = document.querySelector(".fwd");
+const videoPlayer = document.querySelector("#sintel");
+activatePlayer(videoPlayer);
+const audioPlayer = document.querySelector("#tux");
+activatePlayer(audioPlayer);
 
-const timerWrapper = document.querySelector(".timer");
-const timer = document.querySelector(".timer span");
-const timerBar = document.querySelector(".timer div");
+function activatePlayer(player) {
+  const media = player.querySelector(".media");
+  const controls = player.querySelector(".controls");
 
-media.removeAttribute("controls");
-controls.style.visibility = "visible";
+  const play = player.querySelector(".play");
+  const stop = player.querySelector(".stop");
+  const rwd = player.querySelector(".rwd");
+  const fwd = player.querySelector(".fwd");
 
-play.addEventListener("click", playPauseMedia);
+  const timerWrapper = player.querySelector(".timer");
+  const timer = player.querySelector(".timer span");
+  const timerBar = player.querySelector(".timer div");
 
-timer.addEventListener("mouseover", () => {
-  timer.classList.add("can-copy");
-});
+  media.removeAttribute("controls");
+  controls.style.visibility = "visible";
 
-timer.addEventListener("click", () => {
-  if (timer.classList.contains("can-copy")) {
-    navigator.clipboard.writeText(timer.textContent);
-    timer.classList.remove("can-copy");
-  }
-});
+  play.addEventListener("click", playPauseMedia);
 
-const Icons = {
-  PLAY: "P",
-  PAUSE: "u"
-};
+  timer.addEventListener("mouseover", () => {
+    timer.classList.add("can-copy");
+  });
 
-function setIcon(element, icon) {
-  element.setAttribute("data-icon", Icons[icon]);
-}
-
-function playMedia() {
-  media.play();
-  setIcon(play, "PAUSE");
-}
-
-function pauseMedia() {
-  media.pause();
-  setIcon(play, "PLAY");
-}
-
-function playPauseMedia() {
-  if (media.paused) {
-    playMedia();
-  } else {
-    pauseMedia();
-  }
-}
-
-stop.addEventListener("click", stopMedia);
-media.addEventListener("ended", stopMedia);
-
-function stopMedia() {
-  media.pause();
-  media.currentTime = 0;
-  setIcon(play, "PLAY");
-}
-
-document.addEventListener("mousedown", startContinious);
-document.addEventListener("touchstart", startContinious);
-
-document.addEventListener("mouseup", stopContinious);
-document.addEventListener("touchend", stopContinious);
-
-let continiousControls = [];
-
-function stopContinious(event) {
-  for (const control of continiousControls) {
-    if (control.active) {
-      control.stop(event);
+  timer.addEventListener("click", () => {
+    if (timer.classList.contains("can-copy")) {
+      navigator.clipboard.writeText(timer.textContent);
+      timer.classList.remove("can-copy");
     }
-  }
-}
+  });
 
-function startContinious(event) {
-  stopContinious();
-  for (const control of continiousControls) {
-    if (   control.targets?.includes(event.target)
-        || control.elem === event.target) {
-      control.start(event);
-      break;
-    }
-  }
-}
+  const Icons = {
+    PLAY: "P",
+    PAUSE: "u"
+  };
 
-class Mover {
-  active = false;
-  speed = 200;
-
-  constructor(elem) {
-    this.elem = elem;
+  function setIcon(element, icon) {
+    element.setAttribute("data-icon", Icons[icon]);
   }
 
-  startAction() {
-    if (!this.active) {
-      this.active = true;
-      this.elem.classList.add("active");
-      this.action();
-      this.interval = setInterval(() => this.action(), this.speed);
-    }
+  function playMedia() {
+    media.play();
+    setIcon(play, "PAUSE");
   }
 
-  stopAction() {
-    if (this.active) {
-      this.active = false;
-      clearInterval(this.interval);
-      this.elem.classList.remove("active");
-    }
+  function pauseMedia() {
+    media.pause();
+    setIcon(play, "PLAY");
   }
 
-  stop() {
-    this.stopAction();
-    playMedia();
-  }
-
-  start() {
-    pauseMedia();
-    this.startAction();
-  }
-}
-
-const STEP = 5;
-
-const forward = new Mover(fwd);
-forward.action = function () {
-  if (media.currentTime >= media.duration - STEP) {
-    this.stopAction();
-    stopMedia();
-  } else {
-    media.currentTime += STEP;
-  }
-}
-
-const backward = new Mover(rwd);
-backward.action = function () {
-  if (media.currentTime <= STEP) {
-    this.stopAction();
-    stopMedia();
-  } else {
-    media.currentTime -= STEP;
-  }
-}
-
-continiousControls.push(forward);
-continiousControls.push(backward);
-
-
-media.addEventListener("timeupdate", setTime);
-
-function setTime() {
-  /* seconds in hour */
-  const sih = 60 * 60;
-  /* seconds in minute */
-  const sim = 60;
-
-  const hours = Math.floor(media.currentTime / sih);
-  const withoutHours = media.currentTime - hours * sih;
-
-  const minutes = Math.floor(withoutHours / sim);
-  const withoutMinutes = withoutHours - minutes * sim;
-
-  const seconds = Math.floor(withoutMinutes);
-
-  const minuteValue = minutes.toString().padStart(2, "0");
-  const secondValue = seconds.toString().padStart(2, "0");
-  
-  if (hours > 0) {
-    timer.textContent = `${hours}:${minuteValue}:${secondValue}`;
-  } else {
-    timer.textContent = `${minuteValue}:${secondValue}`;
-  }
-
-  const barLength =
-    timerWrapper.clientWidth * (media.currentTime / media.duration);
-  timerBar.style.width = `${barLength}px`;
-}
-
-const timerBarMover = {
-  elem: timerWrapper,
-  targets: [timerWrapper, timerBar],
-  active: false,
-  controller: null,
-
-  start(event) {
-    if (!this.active) {
-      this.active = true;
+  function playPauseMedia() {
+    if (media.paused) {
+      playMedia();
+    } else {
       pauseMedia();
-      this.controller = new AbortController();
-      this.action(event);
-      document.addEventListener(
-        "mousemove", (e) => this.action(e), {signal: this.controller.signal}
-      );
-      document.addEventListener(
-        "touchmove", (e) => this.action(e), {signal: this.controller.signal}
-      );
     }
-  },
+  }
 
-  stop() {
-    if (this.active) {
-      this.active = false;
-      this.controller.abort();
-      this.controller = null;
+  stop.addEventListener("click", stopMedia);
+  media.addEventListener("ended", stopMedia);
+
+  function stopMedia() {
+    media.pause();
+    media.currentTime = 0;
+    setIcon(play, "PLAY");
+  }
+
+  document.addEventListener("mousedown", startContinious);
+  document.addEventListener("touchstart", startContinious);
+
+  document.addEventListener("mouseup", stopContinious);
+  document.addEventListener("touchend", stopContinious);
+
+  let continiousControls = [];
+
+  function stopContinious(event) {
+    for (const control of continiousControls) {
+      if (control.active) {
+        control.stop(event);
+      }
+    }
+  }
+
+  function startContinious(event) {
+    stopContinious();
+    for (const control of continiousControls) {
+      if (   control.targets?.includes(event.target)
+          || control.elem === event.target) {
+        control.start(event);
+        break;
+      }
+    }
+  }
+
+  class Mover {
+    active = false;
+    speed = 200;
+
+    constructor(elem) {
+      this.elem = elem;
+    }
+
+    startAction() {
+      if (!this.active) {
+        this.active = true;
+        this.elem.classList.add("active");
+        this.action();
+        this.interval = setInterval(() => this.action(), this.speed);
+      }
+    }
+
+    stopAction() {
+      if (this.active) {
+        this.active = false;
+        clearInterval(this.interval);
+        this.elem.classList.remove("active");
+      }
+    }
+
+    stop() {
+      this.stopAction();
       playMedia();
     }
-  },
 
-  action(event) {
-    const width = this.elem.getBoundingClientRect().width;
-    const x = this.elem.getBoundingClientRect().x;
-    const perc = Math.min(Math.max(0, (event.clientX - x) / width), 1);
-    media.currentTime = perc * media.duration;
-    timerBar.style.width = `${width * perc}px`;
+    start() {
+      pauseMedia();
+      this.startAction();
+    }
   }
-}
 
-continiousControls.push(timerBarMover);
+  const STEP = 5;
+
+  const forward = new Mover(fwd);
+  forward.action = function () {
+    if (media.currentTime >= media.duration - STEP) {
+      this.stopAction();
+      stopMedia();
+    } else {
+      media.currentTime += STEP;
+    }
+  }
+
+  const backward = new Mover(rwd);
+  backward.action = function () {
+    if (media.currentTime <= STEP) {
+      this.stopAction();
+      stopMedia();
+    } else {
+      media.currentTime -= STEP;
+    }
+  }
+
+  continiousControls.push(forward);
+  continiousControls.push(backward);
+
+
+  media.addEventListener("timeupdate", setTime);
+
+  function setTime() {
+    /* seconds in hour */
+    const sih = 60 * 60;
+    /* seconds in minute */
+    const sim = 60;
+
+    const hours = Math.floor(media.currentTime / sih);
+    const withoutHours = media.currentTime - hours * sih;
+
+    const minutes = Math.floor(withoutHours / sim);
+    const withoutMinutes = withoutHours - minutes * sim;
+
+    const seconds = Math.floor(withoutMinutes);
+
+    const minuteValue = minutes.toString().padStart(2, "0");
+    const secondValue = seconds.toString().padStart(2, "0");
+    
+    if (hours > 0) {
+      timer.textContent = `${hours}:${minuteValue}:${secondValue}`;
+    } else {
+      timer.textContent = `${minuteValue}:${secondValue}`;
+    }
+
+    const barLength =
+      timerWrapper.clientWidth * (media.currentTime / media.duration);
+    timerBar.style.width = `${barLength}px`;
+  }
+
+  const timerBarMover = {
+    elem: timerWrapper,
+    targets: [timerWrapper, timerBar],
+    active: false,
+    controller: null,
+
+    start(event) {
+      if (!this.active) {
+        this.active = true;
+        pauseMedia();
+        this.controller = new AbortController();
+        this.action(event);
+        document.addEventListener(
+          "mousemove", (e) => this.action(e), {signal: this.controller.signal}
+        );
+        document.addEventListener(
+          "touchmove", (e) => this.action(e), {signal: this.controller.signal}
+        );
+      }
+    },
+
+    stop() {
+      if (this.active) {
+        this.active = false;
+        this.controller.abort();
+        this.controller = null;
+        playMedia();
+      }
+    },
+
+    action(event) {
+      const width = this.elem.getBoundingClientRect().width;
+      const x = this.elem.getBoundingClientRect().x;
+      const perc = Math.min(Math.max(0, (event.clientX - x) / width), 1);
+      media.currentTime = perc * media.duration;
+      timerBar.style.width = `${width * perc}px`;
+    }
+  }
+
+  continiousControls.push(timerBarMover);
+}
