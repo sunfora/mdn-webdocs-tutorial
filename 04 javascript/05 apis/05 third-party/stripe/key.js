32e069867432dbb8ac3fcca1189c9e189bddf6da
kurku <sandovin@mail.ru>
2025-09-28 21:31:48 +0300

    fix api soultion

---
+++ 04 javascript/05 apis/05 third-party/stripe/key.js
@@ -0,0 +1,49 @@
+const LOCAL_STORAGE_KEY = 'MAPQUEST-API-KEY';
+
+const background = document.createElement("div");
+const box        = document.createElement("div");
+const message    = document.createElement("p");
+const input      = document.createElement("input"); 
+
+message.textContent = "Insert api key here, then press Enter";
+
+background.appendChild(box);
+box.appendChild(message);
+box.appendChild(input);
+
+background.style.width      = '100vw';
+background.style.position   = 'absolute';
+background.style.top        = '0';
+background.style.left       = '0';
+background.style.height     = '100vh';
+background.style.background = 'rgba(0, 0, 0, 0.5)';
+
+box.style.marginInline = 'auto';
+box.style.background   = 'white';
+box.style.width        = '200px';
+box.style.padding      = '100px';
+box.style.borderRadius = '10px';
+box.style.position     = 'relative';
+box.style.top          = 'calc(50vh - 150px)';
+
+background.addEventListener('keydown', (e) => {
+  console.log(e);
+  if (e.key === 'Enter') {
+    const key = input.value;
+    localStorage.setItem(LOCAL_STORAGE_KEY, key);
+    background.remove();
+
+    const apiKeyEvent = new CustomEvent('apiKey', { detail: { key: key} });
+    document.dispatchEvent(apiKeyEvent);
+  }
+});
+
+document.addEventListener('DOMContentLoaded',  () => {
+  const key = localStorage.getItem(LOCAL_STORAGE_KEY);
+  if (key === null) {
+    document.body.appendChild(background);
+  } else {
+    const apiKeyEvent = new CustomEvent('apiKey', { detail: { key: key} });
+    document.dispatchEvent(apiKeyEvent);
+  }
+});
--- 04 javascript/05 apis/05 third-party/stripe/script.js
+++ 04 javascript/05 apis/05 third-party/stripe/script.js
@@ -1,8 +1,10 @@
-L.mapquest.key = 'YOUR-API-KEY-HERE';
+document.addEventListener('apiKey', e => {
+  L.mapquest.key = e.detail.key;
 
-// 'map' refers to a <div> element with the ID map
-const map = L.mapquest.map('map', {
-  center: [53.480759, -2.242631],
-  layers: L.mapquest.tileLayer('map'),
-  zoom: 12
-});
+  // 'map' refers to a <div> element with the ID map
+  const map = L.mapquest.map('map', {
+    center: [53.480759, -2.242631],
+    layers: L.mapquest.tileLayer('map'),
+    zoom: 12
+  });
+})
